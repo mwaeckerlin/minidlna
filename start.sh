8c058e9d0f7c116825ecc554193da6ea09bb0839
Marc Wäckerlin <marc@waeckerlin.org>
2016-11-18 20:27:29 +0100

    correctly create config file

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #! /bin/bash -ex
 
-if ! test -f /config/minidlna.conf; then
-    cat > /config/minidlna.conf <<EOF
+cat > /config/minidlna.conf <<EOF
 $(for d in  ${MEDIA_DIR}; do
     echo "media_dir=${d}"
   done)
@@ -10,11 +9,9 @@ serial=$(tr -dc '0-9' < /dev/urandom | fold -w 15 | head -n 1)
 album_art_names=${ALBUM_ART_NAMES}
 friendly_name=${FRIENDLY_NAME}
 inotify=ye
-media_dir=${MEDIA_DIR}
 model_name=${MODEL_NAME}
 root_container=${ROOT_CONTAINER}
 user=minidlna
 EOF
-fi
 
 minidlnad ${OPTIONS} -S -f /config/minidlna.conf
